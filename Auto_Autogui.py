da865ccdb1897f344b65c9b85ba083cb78ceca8d
Vinicius George dos Santos <78492236+devscie@users.noreply.github.com>
2021-04-02 21:39:00 -0300

    Add files via upload
    Caso 1 - Automação utilizando biblioteca PyAutoGUI (Python automation library)

---
+++ Auto_Autogui.py
@@ -0,0 +1,33 @@
+import pyautogui
+
+#print(pyautogui.position())
+
+pyautogui.hotkey('ctrl', 'r')
+pyautogui.PAUSE = 2.5
+pyautogui.typewrite("chrome.exe https://www.google.com/")
+pyautogui.PAUSE = 2.5
+pyautogui.click(227, 52)
+pyautogui.typewrite("https://open.spotify.com/")
+pyautogui.typewrite(["enter"])
+pyautogui.PAUSE = 2.5
+pyautogui.click(82, 219)
+pyautogui.PAUSE = 2.5
+pyautogui.click(422, 101)
+pyautogui.typewrite("Iron Maiden")
+pyautogui.typewrite(["enter"])
+pyautogui.PAUSE = 2.5
+pyautogui.hotkey('ctrl', 'f')
+pyautogui.typewrite("Álbuns")
+pyautogui.click(1724, 90)
+pyautogui.typewrite(["enter"])
+pyautogui.PAUSE = 2.5
+pyautogui.hotkey('ctrl', 'f')
+pyautogui.typewrite("Piece of Mind")
+pyautogui.click(1724, 90)
+pyautogui.typewrite(["enter"])
+pyautogui.PAUSE = 2.5
+pyautogui.hotkey('ctrl', 'f')
+pyautogui.typewrite("To tame a land")
+pyautogui.click(1724, 90)
+pyautogui.click(289, 516)
+pyautogui.screenshot('music_spotify.png')
