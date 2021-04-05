7d655311540e00af78e9eb690b4b3629ae220ae1
Vinicius George dos Santos <78492236+devscie@users.noreply.github.com>
2021-04-05 10:07:21 -0300

    Add files via upload
    fix erro automation show view page, slow internet

--- Auto_Autogui.py
+++ Auto_Autogui.py
@@ -2,34 +2,35 @@ import pyautogui
 
 #print(pyautogui.position())
 pyautogui.hotkey('Win', 'm')
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 1.5
 pyautogui.hotkey('Win', 'r')
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 1.5
 pyautogui.typewrite("chrome.exe https://www.google.com/")
 pyautogui.typewrite(["enter"])
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 5
 pyautogui.click(227, 52)
 pyautogui.typewrite("https://open.spotify.com/")
 pyautogui.typewrite(["enter"])
-pyautogui.PAUSE = 2
-pyautogui.click(82, 219)
-pyautogui.PAUSE = 2.5
+pyautogui.PAUSE = 5
+pyautogui.click(78, 220)
+pyautogui.PAUSE = 5
 pyautogui.click(422, 101)
 pyautogui.typewrite("Iron Maiden")
 pyautogui.typewrite(["enter"])
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 5
 pyautogui.hotkey('ctrl', 'f')
-pyautogui.typewrite("Álbuns")
-pyautogui.click(1724, 90)
+pyautogui.typewrite("Albuns")
+pyautogui.click(1661, 90)
 pyautogui.typewrite(["enter"])
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 5
 pyautogui.hotkey('ctrl', 'f')
 pyautogui.typewrite("Piece of Mind")
-pyautogui.click(1724, 90)
+pyautogui.click(1661, 90)
 pyautogui.typewrite(["enter"])
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 5
 pyautogui.hotkey('ctrl', 'f')
 pyautogui.typewrite("To tame a land")
-pyautogui.click(1724, 90)
+pyautogui.click(1661, 90)
 pyautogui.click(289, 516)
+pyautogui.PAUSE = 5
 pyautogui.screenshot('music_spotify.png')
